5bcf0c5981358a9d9a10712a60b25fe3c99c8b47
Andrhan04 <andrham2004@yandex.ru>
2025-04-27 21:00:32 +0800

    add test depend time live on traps count

--- Statistic/depend_timelive_on_trap.py
+++ Statistic/depend_timelive_on_trap.py
@@ -13,17 +13,11 @@ def create_path (all_path : str):
         if not os.path.isdir(curr_path):
             os.mkdir(curr_path)
 
-mem_to_exel_out_null = { 
-        "trap_id" : [], 
-        "trap_cnt" : [], 
-        "time_live" : []
-        }
-
 mem_to_exel = { "trap_id" : [], 
-        "trap_cnt" : [], 
+        "trapsCount" : [], 
         "time_live" : []
         }
-
+cnt_trap : int = 0
 def Get_data(id_pole : int , id_point : int, id_traps : int, exp_id : int, arr : list(int)):
     # открытие файла
     path = f"log_whith_traps\\pole_{id_pole}\\Points_{id_point}\\Traps_{id_traps}\\Statist_{exp_id}.txt"
@@ -36,45 +30,41 @@ def Get_data(id_pole : int , id_point : int, id_traps : int, exp_id : int, arr :
     if(templates["Live_time"] < 0):
         return
     arr.append(templates["Live_time"])
+    global cnt_trap
+    cnt_trap = templates["trapsCount"]
     
 #f = open('Statistic\\config.txt', 'r+')
 id_pole = 0
 id_point = 0
-id_exp = 1
 #f.close()
+
 for id_traps in range(131):
     path = f"log_whith_traps\\pole_{id_pole}\\Points_{id_point}\\Traps_{id_traps}\\"
     arr = []
-    for i in range(2,23):
+    for exp_id in range(2,23):
         Get_data(id_pole, id_point, id_traps, exp_id, arr)
-    mem_to_exel["time_live"].append(mean())
+    mem_to_exel["trap_id"].append(id_traps)
+    mem_to_exel["trapsCount"].append(cnt_traps)
+    mem_to_exel["mean_time_live"].append(arr.mean())
 
-path_to_save = f"Statistic\\log_func\\pole_{id_pole}\\Points_{id_point}\\exp_{id_exp}\\" 
+path_to_save = f"Statistic\\result\\depend_live_time_on_traps\\" 
 create_path(path_to_save)
+#-----------------------Рисуем картинки-----------------------------------------------------------------
 fig, ax = plt.subplots()
-
-ax.plot(mem_to_exel["trap_cnt"], mem_to_exel["time_live"], marker='o', markersize=4)
+ax.plot(mem_to_exel["trapsCount"], mem_to_exel["mean_time_live"], marker='o', markersize=4)
 ax.grid(True, linestyle='-.', linewidth=0.5, color='gray')
 ax.tick_params(axis='both', which='both', labelsize=8, width=1, color='red')
 plt.xlabel('Колличество ловушек') #Подпись для оси х
 plt.ylabel('Время жизни') #Подпись для оси y
-plt.title('Зависимость времени жизни от ловушек при $10^7$ итераций') #Название
+plt.title('Зависимость времени жизни от ловушек при $3 * 10^6$ итераций') #Название
 plt.savefig(path_to_save + f'Grafic_{id_exp}.png')
 plt.show()
 plt.close()
-fig, ax = plt.subplots()
-ax.plot(mem_to_exel_out_null["trap_cnt"], mem_to_exel_out_null["time_live"], marker='o', markersize=4)
-ax.grid(True, linestyle='-.', linewidth=0.5, color='gray')
-ax.tick_params(axis='both', which='both', labelsize=8, width=1, color='red')
-plt.xlabel('Колличество ловушек') #Подпись для оси х
-plt.ylabel('Время жизни') #Подпись для оси y
-plt.title('Зависимость времени жизни от ловушек при $10^7$ итераций') #Название
-plt.savefig(path_to_save + f'Grafic_{id_exp}_out_NULL.png')
-plt.show()
-plt.close()
+
+#------------------------------Запись в excel
 df_x : pd.DataFrame =  pd.DataFrame(mem_to_exel)
 df_x : pd.DataFrame =  pd.DataFrame(mem_to_exel_out_null)
-#------------------------------Запись в excel
-with pd.ExcelWriter(path_to_save + f"Function_{id_exp}.xlsx") as writer:  
+
+with pd.ExcelWriter(path_to_save + f"Function_{id_point}.xlsx") as writer:  
     df_x.to_excel(writer, sheet_name='Data_all')
     df_x.to_excel(writer, sheet_name='Data_out_NULL')
